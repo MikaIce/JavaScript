17399932a1b9ad90fbe63c5b0f721ebba825add6
Mickaice <michael.benoit13@gmail.com>
2016-04-19 14:26:48 +0200

    exo_4

---
+++ intro_javascript/chapitre_1/js/valeurs.js
@@ -0,0 +1,6 @@
+console.log(4 + 5);
+//vas donner le resultat du calcul 4 + 5 = 9.
+console.log("4 + 5");
+//vas ecrire la chaine de carecter sivante 4 + 3.
+console.log("4" + "5");
+//vas additionner les deux chaine de caracter suivante qui donne 45. 
